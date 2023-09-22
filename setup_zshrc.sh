2a3c4d3cc47e82b22224caba941f2f54a5ea7895
ZoneMix <38586893+ZoneMix@users.noreply.github.com>
2023-09-22 08:46:59 -0500

    Update setup_zshrc.sh

--- setup_zshrc.sh
+++ setup_zshrc.sh
@@ -3,5 +3,3 @@
 if ! alias | grep -q "alias msf-run="; then
     echo "alias msf-run=\"docker run -it tleemcjr/metasploitable2:latest sh -c \"/bin/services.sh\"\"" >> ~/.zshrc
 fi
-
-source ~/.zshrc
